cba968bc208d367303f6d12a4df7eda6bbd98d81
kenndraws <ken.castellanos.draws@gmail.com>
2022-04-22 15:24:05 -0600

    Home Edit
    Editing the Home Page to have an appealing animation sliding up and a nice seamless background. Edited Mobile aspects as well

--- Project-3/js/bodies.js
+++ Project-3/js/bodies.js
@@ -4,6 +4,7 @@ function getAPOD() {
     //Fetch Astronomy Photo of the Day from API
     const APOD_URL = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
 
+    //Typical API Call using ASYNC AWAIT to get Astronomy Photo of the Day
     const getPromise = async () => {
         const response = await fetch(APOD_URL);
         const res = await response.json();
@@ -16,21 +17,26 @@ function getAPOD() {
     });
 }
 function getBodies() {
+    //Get data.json for information
     const getPromise = async () => {
         const response = await fetch('./js/data.json');
         const res = await response.json();
         return res;
     }
     getPromise().then(bodies => {
+        //Loop through items in object
         for (const id in bodies) {
+            //Easier Handling
             const body = bodies[id];
 
+            //Decide which parent section it will be added too
             var parent = ".Other";
             if (body.title === "Planet") parent = ".Planets";
             else if (body.title === "Moon") parent = ".Moons";
             else if (body.title === "Dwarf Planet") parent = ".Dwarfs";
             else if (body.title === "Asteroid") parent = ".Asteroids";
 
+            //Grab parent then create info element.
             const host = document.querySelector(parent);
             host.innerHTML += createBodyInfoElement(body);
         }
@@ -38,28 +44,37 @@ function getBodies() {
 }
 
 function createBodyInfoElement(info) {
+    //Returns a string with html representation of element structure
+    //Primarily used to not have repetitive code and automate creation.
+
+    //Logic in body-info includes checking to make sure description isn't too big
+    //Checking if the body is a planet and if so show the number of moons.
+    //if not a planet, check if it is around an object i.e planet show parent.
+    //if not then display if it has moons.
+
     return (
-        `<div class="body-info">
-            <div class="body-info-main">
+        `<section class="body-info">
+            <section class="body-info-main">
                 <img src=${info.img} alt=${info.name} onerror="this.onerror=null; this.src='./img/default.jpg'"class="body-info-image" />
-                <div class="body-info-text">
+                <section class="body-info-text">
                     <h2>${info.title}</h2>
                     <h3>${info.name}</h3>
                     <p><strong>${info.sciName},</strong> 
                         <span> ${(info.description.length > 620 ? info.description.substring(0,616) + "..." : info.description)}</span>
                     </p>
-                </div>
-            </div>
-            <div class="body-info-icons">
+                </section>
+            </section>
+            <section class="body-info-icons">
                 ${(info.isPlanet ? `<p>Moons<br/>${info.moons}</p>` :
             (info.around ? `<p>Parent<br/>${info.around[0].toUpperCase() + info.around.substring(1)}</p>` : `<p>Moons<br/>${info.moons}</p>`)
         )}
                 <p>Gravity<br/>${info.gravity}</p>
                 <p>Mass<br/>${info.mass.massValue.toFixed(2)} x 10 <sup>${info.mass.massExponent}</sup></p>
                 <p>Temp<br/>${info.avgTemp}</p>
-            </div>
-        </div>`
+            </section>
+        </section>`
     );
 }
-getAPOD();
+
+//getAPOD();
 getBodies();
--- Project-3/js/main.js
+++ Project-3/js/main.js
@@ -6,9 +6,11 @@ function main() {
 
 let displayed = true;
 function displayMobileMenu() {
-    const menu = document.querySelector("#Menu");
+    //Get Menu and Menu-Button for Slide Effect
+    const menu = document.querySelector("#Menu"); 
     const button = document.querySelector("#menu-button");
-    const header = document.querySelector("header");
+
+    //If menu slide in or out
     if (displayed) {
         menu.classList.add('slide');
         button.classList.add('button-slide');
@@ -17,25 +19,27 @@ function displayMobileMenu() {
         menu.classList.remove('slide');
         button.classList.remove('button-slide');
     }
+    //Swap menu display true or false
     displayed = !displayed;
 }
 
 function displayClickedMenu() {
+    //Get Menu Item to add event listeners to each input
     const menu = document.querySelector("#Menu");
-    Array.from(menu.children).filter(elem => elem.tagName === "INPUT").map(input => {
-        isChecked(input);
+    //Loop through children who are input type
+    Array.from(menu.children).filter(elem => elem.tagName === "INPUT").forEach(input => {
+        isChecked(input); //Display default checked which is Home
         input.addEventListener("change", () => {
-            isChecked(input);
-            console.log(document.querySelector("#menu-button").getComputedStyle);
-            
+            isChecked(input); //Display Checked
             var mq = window.matchMedia("(max-width: 1200px)");
-            if (mq.matches) displayMobileMenu();
+            if (mq.matches) displayMobileMenu(); //If using mobile then slide menu after click
         });
     });
 
     function isChecked(input) {
-        if (input.checked) {
-            Array.from(document.querySelector("main").children).map(section => {
+        //if input is checked display section corresponding to the button hide rest
+        if (input.checked) { 
+            Array.from(document.querySelector("main").children).forEach(section => {
                 if (section.classList.contains(input.id)) section.style.display = "block";
                 else section.style.display = "none";
             });
