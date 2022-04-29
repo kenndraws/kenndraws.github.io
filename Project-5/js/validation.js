44ceac925659d047e601f914561374d8a7355ca1
kenndraws <ken.castellanos.draws@gmail.com>
2022-04-29 09:37:05 -0600

    Separating Projects

---
+++ Project-5/js/validation.js
@@ -0,0 +1,93 @@
+/* Form Validation Example */
+/* Personal Web Site-Visitor Form Validation */
+/* See comments with TODO for code you need to implement */
+const stateAbbreviations = [
+    'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA',
+    'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA',
+    'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+    'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+    'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'
+];
+
+function initValidation(formName) {
+
+    //$("#myform")
+    let $form = $(formName);
+
+    $('form :input').change(function (ev) {
+        validateForm();
+        if (!this.checkValidity())
+            $(this).addClass("was-validated")
+
+        //NOTE: we use 'was-validated' class so that you show the error 
+        //indications only for the single field rather
+        //than the whole form at once
+    });
+
+    $form.submit(function (event) {
+        $form = $(this);
+        formEl = $form.get(0); //formElement
+
+        event.preventDefault();  //prevent default browser submit
+        event.stopPropagation(); //stop event bubbling
+
+        validateForm();
+
+        if (!formEl.checkValidity()) {
+            $(":input").addClass("was-validated")
+        }
+        else {
+            $form.hide(); //Hide Form
+            $(".successMsg").show(); //display success message
+            
+            var formData = new FormData(formEl)
+            console.log($form.serialize().split('&'));
+
+            $form.each(function(){
+                this.reset();
+            });
+            $(".user-sign").removeClass('paused');
+            main();
+        }
+    });
+}
+
+function validateForm() {
+
+    validateState("#state", "");
+
+    /*note, to validate the group, 
+    just passing in id of one of them ("#newspaper"), 
+    we will use groupName to check status of group.  
+    Just call setElementValidity on the '#newspaper' element to show the error message*/
+
+    validateCheckboxGroup("#newspaper", "find-page", "");
+
+}
+function validateState(id, msg) {
+    $el = $(id);
+    let valid = false;
+    let state = $el.get(0).value.toUpperCase();
+    valid = stateAbbreviations.includes(state);
+
+    setElementValidity(id, valid, msg);
+}
+
+function validateCheckboxGroup(fieldName, groupName, message) {
+    let valid = $(`input[name="${groupName}"]:checked`).length > 0;
+    
+    setElementValidity(fieldName, valid, message);
+
+    return valid;
+}
+
+function setElementValidity(fieldName, valid, message) {
+    let $field = $(fieldName);
+    let el = $field.get(0);
+    if (valid) {  //it has a value
+        el.setCustomValidity('');  //sets to no error message and field is valid
+    } else {    
+        el.setCustomValidity(message);   //sets error message and field gets 'invalid' stat
+    }
+    //TODO  insert or remove message in error div for element
+}
